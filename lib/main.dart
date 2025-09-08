3934dddb7115d7d30cfeb4f9f065c21e8db4f61f
ritanshu <ritanshu@bcoder.co.in>
2025-09-08 16:23:08 +0530

    feat: Integrate Firebase and update splash screen
    bug fixes
    - Configure Firebase for Android and iOS platforms
    - Update Android and iOS splash screens with new assets and styles
    - Implement AuthRepository for user authentication and data management
    - Add `profilePic` field to `SignUpRequested` event
    - Update app name to "Task Flow"
    - Add taskflow_splash.png asset

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'core/widgets/features/auth/logic/bloc/auth_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // if you used FlutterFire CLI
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options:
+        DefaultFirebaseOptions.currentPlatform, // only if using FlutterFire CLI
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
--- lib/utils/bloc_providers.dart
+++ lib/utils/bloc_providers.dart
@@ -1,3 +1,4 @@
+import 'package:bloctodolist/core/widgets/features/auth/repository/auth_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../core/widgets/features/auth/logic/bloc/auth_bloc.dart';
@@ -6,7 +7,10 @@ import '../core/widgets/features/task/logic/bloc/task_bloc.dart';
 
 class AppProviders {
   static List<BlocProvider> get allProviders => [
-    BlocProvider<AuthBloc>(create: (_) => AuthBloc()..add(AppStarts())),
+    BlocProvider<AuthBloc>(
+      create: (_) =>
+          AuthBloc(authRepository: AuthRepository())..add(AppStarts()),
+    ),
     BlocProvider<TaskBloc>(create: (_) => TaskBloc()),
     // add more blocs here
   ];
